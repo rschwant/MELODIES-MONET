cd7a5fc8d5f383445ec2c4d45ed76e59e9b1b003
davidfillmore <fillmore.winslow.david@gmail.com>
2021-10-14 11:34:22 -0600

    Start on a control.yaml file for unit tests.

--- melodies_monet/tests/test_driver.py
+++ melodies_monet/tests/test_driver.py
@@ -1,8 +1,23 @@
 """
 pytest test_driver.py
 """
+import driver
 
-def test_driver():
-    import driver
+"""
+define a variable in the global scope
+to hold a driver.analysis object
+and be shared across unit tests
+"""
+an = None
+
+
+def test_init():
+    global an
     an = driver.analysis()
+    assert True
+
+
+def test_read_control():
+    global an
+    an.read_control()
     assert True
