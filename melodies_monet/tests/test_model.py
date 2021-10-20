8ceb30127879457de78cacb8aae0fc706ef9f7d8
David Fillmore <fillmore@pop-os.localdomain>
2021-10-20 17:26:29 -0600

    Added test_model.test_glob_files.

--- melodies_monet/tests/test_model.py
+++ melodies_monet/tests/test_model.py
@@ -2,6 +2,7 @@
 pytest test_model.py
 """
 import sys
+import numpy as np
 sys.path.insert(0, '..')
 import driver
 
@@ -11,10 +12,20 @@ to hold a driver.model object
 and be shared across unit tests
 """
 model = None
+file_str = '*.nc'
+file_test = 'test_model.nc'
 
 
 def test_init():
     global model
     model = driver.model()
+    model.file_str = file_str
     assert True
 
+
+def test_glob_files():
+    global model
+    model.glob_files()
+    print(model.files)
+    assert type(model.files) is np.ndarray
+    assert model.files[0] == file_test
