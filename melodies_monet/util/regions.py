12e35249ce413fa8f92a15446560fe85da0f4fac
dwfncar <fillmore@ucar.edu>
2021-12-30 16:11:03 -0700

    Start on script to generate regional bounding boxes.

---
+++ melodies_monet/util/regions.py
@@ -0,0 +1,12 @@
+import cartopy.feature as cfeature
+from cartopy.io import shapereader
+
+resolution = '110m'
+category = 'cultural'
+name = 'admin_0_countries'
+
+shapefile = shapereader.natural_earth(resolution, category, name)
+reader = shapereader.Reader(shapefile)
+countries = reader.records()
+for country in countries:
+    print(country.attributes)
